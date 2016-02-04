8d94c9e798abe6eef305054c5974b2a6a549baee
Djczq <jeromedjczq@gmail.com>
2016-02-04 21:45:24 +0100

    voit

--- src/googlecars/Voiture.java
+++ src/googlecars/Voiture.java
@@ -1,5 +1,20 @@
 package googlecars;
 
-public class Voiture {
+import java.util.LinkedList;
 
+public class Voiture {
+	private LinkedList<Intersection> chemin;
+	private int tempsRestant;
+	
+	public void addIntersection(Intersection i){
+		this.chemin.add(i);
+		this.tempsRestant -=
+		
+	}
+	
+	public Voiture(int temps){
+		chemin = new LinkedList<Intersection>();
+		this.tempsRestant=temps;
+	}
+	
 }
